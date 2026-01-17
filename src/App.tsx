5a7d9ea2b0e2c601b2a85803ad21671c5184a963
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2026-01-17 09:51:07 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';       // Halaman LOBBY (Menu Utama)
-import Explore from './pages/Explore';   // Halaman ALAM GAIB (Hutan)
-import City from './pages/City';         // Halaman KOTA (Baru)
+import Index from './pages/Index';
+import City from './pages/City';
+import Explore from './pages/Explore';
+import About from './pages/About'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Menu Utama (Lobby) */}
         <Route path="/" element={<Index />} />
-        
-        {/* Level 1: Kota Karawang */}
         <Route path="/city" element={<City />} />
-        
-        {/* Level 2: Alam Gaib */}
         <Route path="/explore" element={<Explore />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
